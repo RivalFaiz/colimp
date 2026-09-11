a21afba61206e5f9c684aceb46724e8550815fc2
RivalFaiz <rivalfaiz4@gmail.com>
2026-09-11 23:43:58 +0700

    Updates

---
+++ assets/useTitle-DKq98_x9.js
@@ -0,0 +1,1 @@
+import{r as e}from"./react-Dj8HpMw-.js";const i="COLIMP — Olimpiade Matematika Indonesia tingkat Universitas";function o(t){e.useEffect(()=>{document.title=t?`COLIMP - ${t}`:i},[t])}export{o as u};
